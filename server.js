2ea974c172ff4ade79468792cf0e9c59986e4e90
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2025-01-20 22:23:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,15 +21,6 @@ app.use(cors(corsOptions));
 
 // Handle preflight requests
 app.options('*', cors(corsOptions));
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-        return res.status(204).end();
-    }
-    next();
-});
 
 app.use(bodyParser.json());
 
